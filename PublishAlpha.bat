f788cb43d319f1d6aae5fb4ddae194848dab242c
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-06-14 13:26:27 +0200

    Fixed up build scripts;

--- PublishAlpha.bat
+++ PublishAlpha.bat
@@ -7,7 +7,7 @@ set PROJECT=Alpha
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set MYDATE=%%c-%%a-%%b)
 for /f "tokens=1-2 delims=/: " %%a in ('time /t') do (set MYTIME=%%a-%%b)
 
-cd "dist\Debug"
+cd "dist\Debug\net472"
 call "C:\Program Files\WinRAR\rar.exe" a "Kuriimu2_%PROJECT%.rar" "*.exe*" "*.dll" "-x*criware*" "-x*e.x._troopers*" "-x*valkyria_chronicles*" "plugins\*.dll" "-xplugins\Kontract.dll" "-xplugins\Komponent.dll" "-xplugins\Kryptography.dll" "-xplugins\System.Numerics.Vectors.dll" "Libraries"
 cd ../../
 move "dist\Debug\Kuriimu2_%PROJECT%.rar" "Kuriimu2_%PROJECT%_%MYDATE%_%MYTIME%.rar"
